4af0ba2bf5ad15fb7140f51681e082dd601adcaa
Luis Alberto <Luis Alberto@D342114R>
2021-02-21 17:57:20 -0300

    Updates and merge code for routes and path manager

--- Common/Controller/Native-Routes-Whith-jQuery/RouteManager.js
+++ Common/Controller/Native-Routes-Whith-jQuery/RouteManager.js
@@ -27,6 +27,41 @@ class RouteManager {
     }
 
     /**
+     * Get and return the pathname of application
+     * @returns {String} A text with the pathname of application
+     */
+    ApplicationContextPath() {
+        return [
+            "//",
+            window.location.hostname,
+            window.location.pathname.split("/")[1]
+        ].join("/");
+    }
+
+    /**
+     * Get and return the complete pathname of application
+     * @param {String} module A text with name of module
+     * @param {String} complement A text with complement to path
+     * @returns {String} A text with complete pathname
+     */
+    GetModule(module = "", complement = "") {
+        return this.ApplicationContextPath() + "/" + [
+            module,
+            complement
+        ].join("/");
+    }
+
+    /**
+     * Get and return the value of parameter based in the key name
+     * @param {String} name A text with the name of parameter
+     * @returns {String} A text with the value of parameter
+     */
+    GetParameterURL(name) {
+        let Parameter = new URLSearchParams(window.location.search);
+        return Parameter.get(name);
+    }
+
+    /**
      * Redirect or load content based in model of route suplied
      * @returns {void}
      */
